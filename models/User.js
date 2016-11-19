4ef72b5efe0f69d8e03f4db99832dc7464bb282f
Thiago Andrade <thiagoh@gmail.com>
2016-11-19 14:57:48 -0500

    removing unnecessary apis

--- models/User.js
+++ models/User.js
@@ -3,7 +3,10 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
+  email: {
+    type: String,
+    unique: true
+  },
   password: String,
   passwordResetToken: String,
   passwordResetExpires: Date,
@@ -24,18 +27,26 @@ const userSchema = new mongoose.Schema({
     website: String,
     picture: String
   }
-}, { timestamps: true });
+}, {
+  timestamps: true
+});
 
 /**
  * Password hash middleware.
  */
 userSchema.pre('save', function save(next) {
   const user = this;
-  if (!user.isModified('password')) { return next(); }
+  if (!user.isModified('password')) {
+    return next();
+  }
   bcrypt.genSalt(10, (err, salt) => {
-    if (err) { return next(err); }
+    if (err) {
+      return next(err);
+    }
     bcrypt.hash(user.password, salt, null, (err, hash) => {
-      if (err) { return next(err); }
+      if (err) {
+        return next(err);
+      }
       user.password = hash;
       next();
     });
