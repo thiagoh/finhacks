fccfe0a71efa3c6c2a15683340c9f901d471ec4a
Thiago Andrade <thiagoh@gmail.com>
2016-11-19 17:06:36 -0500

    partials working

--- controllers/home.js
+++ controllers/home.js
@@ -2,12 +2,21 @@
  * GET /
  * Home page.
  */
-exports.index = (req, res) => {
-	res.render('home', {
-		title: 'Home',
-		user: {
-			firstName: 'Andre',
-			lastName: 'Oliveira',
-		}
-	});
+
+
+exports.index = function(app, engine) {
+
+	return (req, res) => {
+
+		res.render('home', {
+			title: 'Home',
+			user: {
+				firstName: 'Andre',
+				lastName: 'Oliveira',
+			},
+			partials: Promise.resolve({
+				jsIncludes: engine.handlebars.compile('{{>home-scripts}}')
+			})
+		});
+	};
 };
